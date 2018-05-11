b9d72504539f1f0ee4d41aa14a545df7a2b45f5b
Elad Hoffer <elad.hoffer@gmail.com>
2018-05-11 10:46:06 +0300

    resnet_quantized_float_bn

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,7 @@
 from .alexnet import *
 from .resnet import *
 from .resnet_quantized import *
+from .resnet_quantized_float_bn import *
 from .resnext import *
 from .inception_resnet_v2 import *
 from .inception_v2 import *
